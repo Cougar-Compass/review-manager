e940b45e4b9e7a2f516b6604d11aa1b1bff50f1c
Joseph Jophy <79068629+josephjophy@users.noreply.github.com>
2023-09-03 20:35:14 -0500

    created connection to cassandra db

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gocql/gocql"
 )
 
 func main() {
-	// Define a simple HTTP handler function
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
+	// Create a connection to Cassandra
+	cluster := gocql.NewCluster("localhost")
+	cluster.Keyspace = "university_of_houston"
+	session, err := cluster.CreateSession()
+	if err != nil {
+		log.Printf("Error connecting to Cassandra: %v", err)
+		return
+	}
+	defer session.Close()
+
+	fmt.Println("Connected to Cassandra!")
 
 	// Start the HTTP server on port 8080
 	fmt.Println("Server is running on :8080")
